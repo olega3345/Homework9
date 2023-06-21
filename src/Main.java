39f173ea9c76755d5625895655cf39aa9a4ae605
Олег <olegcelysev778@gmail.com>
2023-06-21 18:58:33 +0300

    Домашка9

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,62 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        task1();
+        task2();
+        task3();
+        task4();
+    }
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+    public static void task1() {
+        int[] arr = generateRandomArray();
+        System.out.println("Задача 1");
+        int sum = 0;
+        for (int index = 0; index < arr.length; index++) {
+            sum = sum + arr[index];
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+    }
+    public static void task2() {
+        int[] arr = generateRandomArray();
+        System.out.println("Задача 2");
+        int maxExpenditure = 99000;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > maxExpenditure) {
+                maxExpenditure = arr[i];
+            }
+        }
+        int minExpenditure = 201000;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < minExpenditure) {
+                minExpenditure = arr[i];
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + minExpenditure + " рублей. Максимальная сумма трат за день составила " + maxExpenditure + " рублей");
+    }
+    public static void task3() {
+        int[] arr = generateRandomArray();
+        System.out.println("Задача 3");
+        float sum = 0;
+        float averageAmountSpent = 0;
+        for (int index = 0; index < arr.length; index++) {
+            sum = sum + arr[index];
+        }
+        averageAmountSpent = sum / 30;
+        System.out.println("Средняя сумма трат за месяц составила " + averageAmountSpent + " рублей");
+    }
+    public static void task4() {
+        int[] arr = generateRandomArray();
+        System.out.println("Задача 4");
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0 ; i--) {
+            System.out.print(reverseFullName[i]);
         }
     }
 }
