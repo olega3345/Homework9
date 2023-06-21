778b9c5f36be849eec378d3a93623b9bbd9def27
Олег <olegcelysev778@gmail.com>
2023-06-21 19:00:16 +0300

    Домашка9

--- src/Main.java
+++ src/Main.java
@@ -56,7 +56,7 @@ public class Main {
         System.out.println("Задача 4");
         char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
         for (int i = reverseFullName.length - 1; i >= 0 ; i--) {
-            System.out.print(reverseFullName[i]);
+            System.out.print(reverseFullName[i] );
         }
     }
 }
